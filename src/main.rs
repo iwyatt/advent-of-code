84489203d20c646f3c0feddfb967a86a5a4329f2
iwyatt <7514713+iwyatt@users.noreply.github.com>
2025-12-04 12:47:54 -0800

    completed day 3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-mod aoc25d2p1;
-mod aoc25d2p2;
+mod aoc25d3p1;
+mod aoc25d3p2;
 
 fn main() {
-    // aoc25d2p1::run_example();
-    // aoc25d2p1::run_part1();
-    // aoc25d2p2::run_example();
-    aoc25d2p2::run_part2();
+    // aoc25d3p1::run_example();
+    // aoc25d3p1::run_part1();
+    // aoc25d3p2::run_example();
+    aoc25d3p2::run_part2();
 }
